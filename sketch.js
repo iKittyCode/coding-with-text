650a0c0a2b4e60d7bcf086ed80fc7b0f0623513c
iKittyCode <gator.syon@gmail.com>
2020-11-14 14:48:42 -0500

    Correcting some mistakes

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ function setup() {
 
 function draw() {
   background(220);
-     for (let i = 0; i < 4; i++) {
+     for (let i = 0; i < textfile.length - 1; i++) {
   document.body.outerHTML = textfile[index]
     
 }
